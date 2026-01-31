8ddf1b13734f974ed91c67e0d64281fd3bbb040f
krishnagajera2907-cpu <krishnagajera2907@gmail.com>
2026-01-31 08:19:21 +0530

    Add files via upload

---
+++ ios.py
@@ -0,0 +1,13 @@
+print("hello,world")
+
+name="abc"
+age=15
+print("name:",name)
+print("age:",age)
+
+name=input("enter your name:")
+print("hello",name)
+
+first=input("enter first word:")
+second=input("enter second word:")
+print("you typed:",first,second)
