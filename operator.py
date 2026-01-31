4c2433803614d41612b91b8beb3d60e295db06f6
krishnagajera2907-cpu <krishnagajera2907@gmail.com>
2026-01-31 08:16:59 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,46 @@
+a=7
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a^b)
+
+=3
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+
+print(b/a)
+print(b//a)
+print(b%a)
+print(b**a)
+
+x=true
+y=false
+print(x and y)
+print(x or y)
+print(x not y)
+
+p=10
+q=5
+print(p<q)
+print(p==q)
+
+is_pass=true
+print(is_pass)
+
+a=5
+b=4
+print(a&b)
+print(a/b)
+
+numbers=[1,2,3,4,5]
+print(3 in numbers)
+print(6 not in numbers)
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
