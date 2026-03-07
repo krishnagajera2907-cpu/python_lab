4e8111f2e869401250a712bb14863d743ce400fd
krishnagajera2907-cpu <krishnagajera2907@gmail.com>
2026-03-07 09:08:48 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,16 @@
+from array import array
+arr=array('i',[10,20,30,40,50])
+print (arr[0])#first element
+print (arr[2])#third element
+print (arr[4])#fourth element
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print (arr[-1])#last element
+print (arr[-2])#second element
+print (arr[-4])#first element
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr [2] = 35
+print(arr)
